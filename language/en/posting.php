168b544ac6b4cf5dcc9dba01d8631c1f3fd94773
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2019-10-25 02:37:48 -0500

    Add posting box button

---
+++ language/en/posting.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Mermaid extension for phpBB.
+ * @author Alfredo Ramos <alfredo.ramos@yandex.com>
+ * @copyright 2019 Alfredo Ramos
+ * @license GPL-2.0-only
+ */
+
+/**
+ * @ignore
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+ * @ignore
+ */
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+$lang = array_merge($lang, [
+	'MERMAID_BUTTON_EXPLAIN' => 'Mermaid diagram: [mermaid]diagram code[/mermaid]'
+]);
